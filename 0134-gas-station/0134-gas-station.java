15f54254e9e96a70429508283a7eab0a92de7c55
Shivani Giri <42980529+shivanigiri@users.noreply.github.com>
2023-11-11 20:25:27 +0530

    Time: 1 ms (100%), Space: 55.3 MB (37.68%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,6 +1,7 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int pfsum=0,maxValue=0,index=0;
+        //start pfsum frm backward else we will store first post val, but we need last max val
         for(int i=gas.length-1;i>=0;i--){
             pfsum += gas[i]-cost[i];
             if(maxValue<pfsum){
